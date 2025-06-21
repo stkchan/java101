0d9e7a8aabe58a5ef992b187ca9d19eaf65ff593
stkchan <stkza007hod@hotmail.com>
2025-06-21 17:56:58 +0700

    Add ternaryoperator file

---
+++ ternaryoperator.java
@@ -0,0 +1,8 @@
+public class ternaryoperator {
+    public static void main(String[] args) {
+        int age = 20;
+        String status = (age < 18) ? "Minor" : "Adult";
+        
+        System.out.println("Status: " + status);
+    }
+}
