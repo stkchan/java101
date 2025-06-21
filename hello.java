4c25bdfd0e867bead10b4ef96e1d81b257d81e04
stkchan <stkza007hod@hotmail.com>
2025-06-21 12:54:31 +0700

    Add more primative variable

--- hello.java
+++ hello.java
@@ -1,7 +1,11 @@
 class Hello {
     public static void main(String[] args) {
-     int age = 20;    
+     int    age1 = 20;
+     float  age2 = 20.2f;
+     double age3 = 20.00;   
         System.out.println("Hello Mundo");
-        System.out.println(age);
+        System.out.println(age1);
+        System.out.println(age2);
+        System.out.println(age3);
     }
 }
