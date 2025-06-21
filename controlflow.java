0e7feaf9c56f8fe7585cef257fd9aa42889fcf4c
stkchan <stkza007hod@hotmail.com>
2025-06-21 15:08:14 +0700

    Add controlflow & switchcase files

---
+++ controlflow.java
@@ -0,0 +1,15 @@
+public class controlflow {
+    public static void main(String[] args) {
+        int num1 = 7;
+
+        if (num1 > 10) {
+            System.out.println("num1 is > than 10");
+        } else if (num1 > 5) {
+            System.out.println("num1 is > than 5");
+            
+        } else {
+            System.out.println("num1 is < than 10");
+        }
+
+    }
+}
---
+++ switchcase2.java
@@ -0,0 +1,26 @@
+public class switchcase2 {
+    public static void main(String[] args) {
+        int day =3;
+
+        // Arrow syntax
+        switch (day) {
+            case 1 -> System.out.println("Monday");
+            case 2 -> System.out.println("Tuesday");
+            case 3 -> System.out.println("Wednesday");
+            case 4 -> System.out.println("Thursday");
+            case 5 -> System.out.println("Friday");
+            case 6 -> System.out.println("Saturday");
+            case 7 -> System.out.println("Sunday");
+            default-> System.out.println("Invalid day");
+        }
+
+        // Switch expression with yield
+        String dayType = switch (day) {
+            case 1, 2, 3, 4, 5 -> "Weekday";
+            case 6, 7 -> "Weekend";
+            default -> "Invalid day";
+        };
+
+        System.out.println("Day Type: " + dayType);
+    }
+}
