944b417e51896119119888623ba08fb1699a14b3
stkchan <stkza007hod@hotmail.com>
2025-06-21 13:43:04 +0700

    again

--- cal.java
+++ cal.java
@@ -5,6 +5,7 @@ public class cal {
 
         float summary1 = (float) (num1 + num2); // Casting type
         float summary2 = (float) num1 / (float) num2; // Casting type
+        String summary3 = "Result = " + summary1; // Convert
         System.out.println(num1 + num2);
         System.out.println(num1 - num2);
         System.out.println(num1 * num2);
@@ -12,6 +13,6 @@ public class cal {
         System.out.println(num1 % num2);
         System.out.println(summary1);
         System.out.println(summary2);
-        System.out.println("Result = " + summary1);
+        System.out.println(summary3);
     }
 }
