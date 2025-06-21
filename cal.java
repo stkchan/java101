05886d745d31fdc8abffbff7a2e8d50b611a5387
stkchan <stkza007hod@hotmail.com>
2025-06-21 13:34:03 +0700

    Add file cal.java

---
+++ cal.java
@@ -0,0 +1,11 @@
+public class cal {
+    public static void main(String[] args) {
+        int num1 = 7;
+        int num2 = 3;
+        System.out.println(num1 + num2);
+        System.out.println(num1 - num2);
+        System.out.println(num1 * num2);
+        System.out.println(num1 / num2);
+        System.out.println(num1 % num2);
+    }
+}
