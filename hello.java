3457426c66d0ab1267d8526c04363624127e312d
stkchan <stkza007hod@hotmail.com>
2025-06-21 13:26:48 +0700

    Add String type

--- hello.java
+++ hello.java
@@ -2,8 +2,11 @@ class Hello {
     public static void main(String[] args) {
      int    age1 = 20;
      float  age2 = 20.2f;
-     double age3 = 20.00;   
+     double age3 = 20.00;
+     String name = "Mundo";  
         System.out.println("Hello Mundo");
+        System.out.println("Hello" + " "+ name);
+        System.out.println("Hello" + " "+ name.toLowerCase());
         System.out.println(age1);
         System.out.println(age2);
         System.out.println(age3);
