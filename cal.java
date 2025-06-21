0e6b956efbc5afea73e736e1c4ec85cde242c870
stkchan <stkza007hod@hotmail.com>
2025-06-21 13:36:47 +0700

    Add Casting Type

--- cal.java
+++ cal.java
@@ -2,10 +2,13 @@ public class cal {
     public static void main(String[] args) {
         int num1 = 7;
         int num2 = 3;
+
+        float summary = (float) (num1 + num2); // Casting type
         System.out.println(num1 + num2);
         System.out.println(num1 - num2);
         System.out.println(num1 * num2);
         System.out.println(num1 / num2);
         System.out.println(num1 % num2);
+        System.out.println(summary);
     }
 }
